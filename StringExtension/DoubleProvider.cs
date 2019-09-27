1903fb6ef87a3cffaefc5360d2c13f214e1acf27
Marina Sokolova <sokolova.m.e9@gmail.com>
2019-09-27 16:56:52 +0300

    update DoubleProvider class

--- StringExtension/DoubleProvider.cs
+++ StringExtension/DoubleProvider.cs
@@ -1,13 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace StringExtension
 {
+    /// <summary>
+    /// DoubleProvider class
+    /// </summary>
+    /// <seealso cref="System.IFormatProvider" />
+    /// <seealso cref="System.ICustomFormatter" />
     public class DoubleProvider : IFormatProvider, ICustomFormatter
     {
         IFormatProvider parentProvider;
@@ -18,6 +19,16 @@ namespace StringExtension
             parentProvider = parent;
         }
 
+        /// <summary>
+        /// Converts the value of a specified object to an equivalent string representation using specified format and culture-specific formatting information.
+        /// </summary>
+        /// <param name="format">A format string containing formatting specifications.</param>
+        /// <param name="arg">An object to format.</param>
+        /// <param name="formatProvider">An object that supplies format information about the current instance.</param>
+        /// <returns>
+        /// The string representation of the value of <paramref name="arg" />, formatted as specified by <paramref name="format" /> and <paramref name="formatProvider" />.
+        /// </returns>
+        /// <exception cref="FormatException"></exception>
         public string Format(string format, object arg, IFormatProvider formatProvider)
         {
             if (arg == null || format != "IEEE")
@@ -33,6 +44,13 @@ namespace StringExtension
             throw new FormatException(String.Format("The format of '{0}' is invalid.", format));
         }
 
+        /// <summary>
+        /// Returns an object that provides formatting services for the specified type.
+        /// </summary>
+        /// <param name="formatType">An object that specifies the type of format object to return.</param>
+        /// <returns>
+        /// An instance of the object specified by <paramref name="formatType" />, if the <see cref="T:System.IFormatProvider" /> implementation can supply that type of object; otherwise, <see langword="null" />.
+        /// </returns>
         public object GetFormat(Type formatType)
         {
             if (formatType == typeof(ICustomFormatter))
